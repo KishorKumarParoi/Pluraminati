0a39f4fcdb63b1cdadb6465414a43f54747391f2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-12 15:53:53 +0600

    added banner image

--- app/site/page.tsx
+++ app/site/page.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default async function Home() {
   return (
-    <main className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
+    <section className="min-h-screen w-full pt-28 relative flex items-center justify-center flex-col">
       {/* Background Grid */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
 
@@ -11,15 +13,36 @@ export default async function Home() {
 
       {/* Title */}
       <div className="bg-gradient-to-r from-primary via-purple-500 to-secondary-foreground text-transparent bg-clip-text">
-        <h1 className="text-6xl md:text-[300px] font-bold text-center">
+        <h1 className="text-7xl md:text-[100px] lg:text-[200px] xl:text-[300px] font-bold text-center">
           Plura
         </h1>
       </div>
       <div className="bg-gradient-to-r from-primary via-blue-500 to-secondary-foreground text-transparent bg-clip-text">
-        <h1 className="text-6xl md:text-[300px] font-bold text-center">
+        <h1 className="text-7xl md:text-[100px] lg:text-[200px] xl:text-[300px] font-bold text-center ">
           Minati
         </h1>
       </div>
-    </main>
+      <div className="flex justify-center items-center relative md:mt-[-30px]">
+        <Image
+          src={"/assets/preview.avif"}
+          width={1200}
+          height={1200}
+          alt="banner image"
+        />
+      </div>
+      {/* <div className="flex justify-center items-center relative md:mt-[-70px]">
+        <Image
+          src={"/assets/preview.jpeg"}
+          alt="banner image"
+          height={1200}
+          width={1200}
+          className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+        />
+        .
+        <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10">
+          k
+        </div>
+      </div> */}
+    </section>
   );
 }
